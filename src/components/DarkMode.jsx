3781a04bf7a1aaab01e52222de916f7ac0b69f0e
jaber saraj <jaber2x@gmail.com>
2024-08-31 19:27:25 +0330

    Add Local Storage to the project

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,20 +1,37 @@
 import Cards from './Cards'
 import Header from './Header'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function DarkMode() {
 
-    const [value, setValue] = useState(false);
+    const getMode = () => {
+        // const windowDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        // if (windowDark) {
+        //     return true
+        // } else {
+        //     return false
+        // }
 
-    return (
-        <>
-            <div className={`${value ? 'bg-gray-700 text-gray-100' : 'bg-gray-100 text-gray-800'} h-screen`}>
-                <Header value={value} setValue={setValue} />
-                <h2 className='flex justify-center text-xl py-10'>{value ? "Dark Mode" : "Light Mode"}</h2>
-                <Cards value={value} />
-            </div >
-        </>
-    )
+        return JSON.parse(localStorage.getItem('mode'))
+
+}
+
+
+const [value, setValue] = useState(getMode());
+
+useEffect(() => {
+    localStorage.setItem('mode', JSON.stringify(value))
+}, [value])
+
+return (
+    <>
+        <div className={`${value ? 'bg-gray-700 text-gray-100' : 'bg-gray-100 text-gray-800'} h-screen`}>
+            <Header value={value} setValue={setValue} />
+            <h2 className='flex justify-center text-xl py-10'>{value ? "Dark Mode" : "Light Mode"}</h2>
+            <Cards value={value} />
+        </div >
+    </>
+)
 }
 
 export default DarkMode
